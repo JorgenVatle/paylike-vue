44de4cf96b2b56db23e5ac147555a079d23c53f0
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-20 16:06:33 +0300

    Update Paylike instance declaration approach.

--- src/PaylikeSdk.d.ts
+++ src/PaylikeSdk.d.ts
@@ -1,25 +1,26 @@
-declare module Paylike {
-
-    interface PopupField {
-        name: string,
-        label: string,
-        type: string,
-        required: boolean,
-        value: any,
-    }
-
-    interface PopupOptions {
-        currency: string,
-        amount: number,
-        title: string,
-        description: string,
-        locale: string,
-        descriptor: string,
-        custom: any,
-        fields: Array<PopupField>,
-    }
-
-    type PopupCallback = (error: Error, response: any) => void;
-
-    function popup(config: PaymentOptions, callback: PopupCallback): void;
+export interface PopupField {
+    name: string,
+    label: string,
+    type: string,
+    required: boolean,
+    value: any,
 }
+
+export interface PopupOptions {
+    currency: string,
+    amount: number,
+    title: string,
+    description: string,
+    locale: string,
+    descriptor: string,
+    custom: any,
+    fields: Array<PopupField>,
+}
+
+export type PopupCallback = (error: Error, response: any) => void;
+
+export interface PaylikeInstance {
+    popup(config: PaymentOptions, callback: PopupCallback): void
+}
+
+declare function Paylike(apiKey: string): PaylikeInstance;
