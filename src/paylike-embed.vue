e7439f9e0fddc8c08bf4c6a7b5828ed2611adfa9
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-20 13:21:23 +0300

    Add base Paylike-Embed Vue component.

---
+++ src/paylike-embed.vue
@@ -0,0 +1,17 @@
+<template>
+    <form @submit.prevent="submit">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'paylike-embed',
+
+        methods: {
+            submit() {
+                // Handle form submit.
+            }
+        }
+    }
+</script>
