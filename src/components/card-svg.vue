8cd8aa386761fc6c4d6a5b60a5f89a5ce4719ab7
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-21 13:26:32 +0300

    Add card SVG Vue component.

---
+++ src/components/card-svg.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="paylike-card-icon" v-html=""></div>
+</template>
+
+<script>
+    import BlankCard from '../assets/card-blank.svg';
+    import MasterCard from '../assets/card-mastercard.svg';
+    import VisaCard from '../assets/card-visa.svg';
+
+    const availableCards = [
+        {
+            name: 'visa',
+            svg: VisaCard,
+        },
+        {
+            name: 'mastercard',
+            svg: MasterCard,
+        },
+        {
+            name: 'blank',
+            svg: BlankCard,
+        }
+    ];
+
+    export default {
+        name: 'card-svg',
+        props: {
+            card: {
+                default: 'blank',
+                validator(card) {
+                    return !!availableCards.find((available) => available.name === card);
+                }
+            },
+        },
+
+        getHtml(cardName) {
+            return availableCards.find((card) => {
+                return card.name === cardName;
+            }).svg;
+        }
+    }
+</script>
