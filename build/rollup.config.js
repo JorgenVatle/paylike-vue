932c913463859bfcfc15fbff2ab8289384e4d4f6
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-21 12:18:51 +0300

    Use Rollup config to determine output formats and paths.
    This enables us to use watch mode for development purposes.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -2,12 +2,19 @@ import Vue from 'rollup-plugin-vue';
 import Buble from 'rollup-plugin-buble';
 import Typescript from 'rollup-plugin-typescript';
 
+const output = {
+    name: 'PaylikeVue',
+    exports: 'named',
+    file: 'dist/paylike-vue',
+};
+
 export default {
     input: 'src/index.ts',
-    output: {
-        name: 'PaylikeVue',
-        exports: 'named',
-    },
+    output: [
+        {...output, format: 'umd',  file: output.file + '.umd.js'},
+        {...output, format: 'es',   file: output.file + '.esm.js'},
+        {...output, format: 'iife', file: output.file + '.min.js'},
+    ],
     plugins: [
         Typescript(),
         Vue({ compileTemplate: true }),
