79428d569c6ef2a64ee1b59dd45cb6e67e8e9b58
Jørgen Vatle <Jorgen@WcAServices.net>
2018-10-21 15:00:54 +0300

    Add Vue input wrapper component - intended for usage within paylike-embed.

---
+++ src/components/paylike-input.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input v-on="listeners" :value="value" v-bind="attributes">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'paylike-input',
+        inheritAttrs: false,
+        props: ['value'],
+
+        computed: {
+            listeners() {
+                return {
+                    ...this.$listeners,
+                    input: (event) => {
+                        this.$emit('input', event.target.value);
+                    }
+                }
+            },
+
+            attributes() {
+                const attrs = {...this.$attrs};
+
+                delete attrs.name;
+
+                return attrs;
+            }
+        }
+    }
+</script>
